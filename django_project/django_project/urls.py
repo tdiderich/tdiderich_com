50d06276a709eab5400262bddd4a785bf282f0f8
Tyler Diderich <tylerdiderich@Tylers-MBP.hsd1.ca.comcast.net>
2017-03-20 19:56:50 -0700

    retry

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -1,12 +1,10 @@
-from django.conf.urls import patterns, include, url
-
+from django.conf.urls import url, include
 from django.contrib import admin
+from tdiderich import views
+
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'django_project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-)
+    url(r'^$', views.home),
+]
