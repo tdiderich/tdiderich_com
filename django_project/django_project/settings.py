016efc7754be9321e92e5080330d4aaf36b1915b
Tyler Diderich <tylerdiderich@Tylers-MacBook-Air.local>
2017-03-21 15:47:06 -0700

    added www.tdiderich.com

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -25,7 +25,7 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = ['104.236.148.144', 'tdiderich.com']
+ALLOWED_HOSTS = ['104.236.148.144', 'tdiderich.com', 'www.tdiderich.com']
 
 
 # Application definition
