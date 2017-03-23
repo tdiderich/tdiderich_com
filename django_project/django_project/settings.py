860b2dd111563b84e1ebeacb7a9d1c1903531115
Tyler Diderich <tylerdiderich@Tylers-MBP.hsd1.ca.comcast.net>
2017-03-22 20:50:20 -0700

    fix

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -54,6 +54,21 @@ ROOT_URLCONF = 'django_project.urls'
 WSGI_APPLICATION = 'django_project.wsgi.application'
 
 #templates
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': ['templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 #TEMPLATE_DIRS = (
 #    join(BASE_DIR,  'templates'),
