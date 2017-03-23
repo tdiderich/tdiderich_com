b0c941164fac495d22dc5643be3011b09f98b2ef
Tyler Diderich <tylerdiderich@Tylers-MBP.hsd1.ca.comcast.net>
2017-03-22 21:39:13 -0700

    fix

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tdiderich',
+    'static',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -106,3 +107,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
