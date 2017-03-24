68240aef75b02a040c7a86c8d2a927ed7eb331eb
Tyler Diderich <tylerdiderich@Tylers-MBP.hsd1.ca.comcast.net>
2017-03-23 21:11:59 -0700

    style

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -107,12 +107,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    '/home/django/django_project/static',
-)
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
