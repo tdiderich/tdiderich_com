41e31f575475f9991d3dcf0333e288e21610023a
Tyler Diderich <tylerdiderich@Tylers-MacBook-Air.local>
2017-03-24 12:30:17 -0700

    static url

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from tdiderich import views
+from django.conf.urls.static import static
 
 admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.home),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
