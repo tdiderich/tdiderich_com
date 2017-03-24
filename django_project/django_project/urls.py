f5db09aa0d0f11a4d3dae2b3e1784bc471532110
Tyler Diderich <tylerdiderich@Tylers-MBP.hsd1.ca.comcast.net>
2017-03-23 20:39:20 -0700

    style

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -1,13 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from tdiderich import views
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.home),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-urlpatterns += staticfiles_urlpatterns()
+]
