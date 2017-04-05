ca9dba6d09e218869f80adc6b402cd553b2003d1
Tyler Diderich <tylerdiderich@Tylers-MBP.hsd1.ca.comcast.net>
2017-04-04 19:46:12 -0700

    add pages

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -9,6 +9,6 @@ admin.autodiscover()
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.home),
-    url(r'^about/', views.about)
-    url(r'^contact/', views.contact)
+    url(r'^about/', views.about, name='about'),
+    url(r'^contact/', views.contact, name='contact')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
